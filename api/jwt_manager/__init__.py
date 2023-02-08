977df9844dc924f90fa13925c67943f62e4d8e34
Rodolfo <rodolfougaldeochoa@gmail.com>
2023-02-07 18:34:17 -0600

    :fire: [***] ESTRUCTURA DE PRUEBA - DE QUI PARA ATRAS

--- api/jwt_manager/__init__.py
+++
@@ -1,1 +0,0 @@
-from .token_manager import create_token, validate_token
--- api/main.py
+++ api/main.py
@@ -1,40 +1,85 @@
 """
-Script de FastAPI de hello word
-In Python 3.10.6
-Run:
-    uvicorn main:app --reload
+Ejemplo de FastAPI para AWS Lambda
 """
-# import os
-# import sys
-#
-# sys.path.append(os.path.join(os.path.dirname(__file__)))
 
-# fastpi
-from fastapi import FastAPI
+# python
+import json
+import os
+import random
+from pathlib import Path
+from uuid import uuid4
 
-# Mangum aws lambda
+# fastapi
+from typing import Literal, Optional
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
+
+# mangum
 from mangum import Mangum
 
-from .routers import sign_in, users, movies, authorization
+# BOOKS_FILE = "books.json"
+# BOOKS = []
 
-app = FastAPI(
-    # root_path="/api/v1",
-    openapi_url="/api/v1/openapi.json",
-    title="My App",
-    version="0.0.1",
-    description="My description",
-    contact={"name": "mack", "url": "http://mack.host"},
-    license_info={
-        "name": "Apache 2.0",
-        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
-    },
-)
+ROOT_DIR = Path(__file__).resolve(strict=True).parent
+BOOKS_FILE = ROOT_DIR / "books.json"
 
 
-app.include_router(users.router)
-app.include_router(sign_in.router)
-app.include_router(movies.router)
-app.include_router(authorization.router)
+class Book(BaseModel):
+    name: str
+    genre: Literal["fiction", "non-fiction"]
+    price: float
+    book_id: Optional[str] = uuid4().hex
 
-# aws lambda
-handler = Mangum(app=app)
+
+if os.path.exists(BOOKS_FILE):
+    with open(BOOKS_FILE, "r") as f:
+        BOOKS = json.load(f)
+
+app = FastAPI()
+
+lambda_handler = Mangum(app)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to my bookstore app!"}
+
+
+@app.get("/random-book")
+async def random_book():
+    return random.choice(BOOKS)
+
+
+@app.get("/list-books")
+async def list_books():
+    return {"books": BOOKS}
+
+
+@app.get("/book_by_index/{index}")
+async def book_by_index(index: int):
+    if index < len(BOOKS):
+        return BOOKS[index]
+    else:
+        raise HTTPException(404, f"Book index {index} out of range ({len(BOOKS)}).")
+
+
+@app.post("/add-book")
+async def add_book(book: Book):
+    book.book_id = uuid4().hex
+    json_book = jsonable_encoder(book)
+    BOOKS.append(json_book)
+
+    with open(BOOKS_FILE, "w") as f:
+        json.dump(BOOKS, f)
+
+    return {"book_id": book.book_id}
+
+
+@app.get("/get-book")
+async def get_book(book_id: str):
+    for book in BOOKS:
+        if book.book_id == book_id:
+            return book
+
+    raise HTTPException(404, f"Book ID {book_id} not found in database.")
